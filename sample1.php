7965460e81c660e0715fb00ceb05b0cd0c38a7fd
Inoue Shinya <inosdevelopment@gmail.com>
2016-11-25 07:36:57 +0000

    add html and php files

---
+++ sample1.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>Cloud9でPHPを含むHTMLの利用</title>
+</head>
+<body>
+    <p><?php echo "Cloud9でPHPを含むHTMLを利用する。"; ?></p>
+</body>
+</html>
---
+++ sample2.php
@@ -0,0 +1,3 @@
+<?php
+// Cloud9で純粋なPHPの実行
+print "Cloud9で純粋なPHPを利用する。";
